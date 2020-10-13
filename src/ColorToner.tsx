0ff20c87fb29bdd1173ca873372c418499db081a
Fatemeh Khosravi <uni.khosravi@gmail.com>
2020-10-13 09:04:23 +0330

    feat: add required components

--- src/ColorToner.tsx
+++ src/ColorToner.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import TextboxColor from "./components/TextboxColor";
+import SliderColor from "./components/SliderColor";
+import ListColor from "./components/ListColor";
 
 function ColorToner() {
-  return <div></div>;
+  return (
+    <div>
+      <TextboxColor />
+      <SliderColor />
+      <ListColor />
+    </div>
+  );
 }
 
 export default ColorToner;
---
+++ src/components/ListColor.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+const ListColor = () => {
+  return <div></div>;
+};
+export default ListColor;
