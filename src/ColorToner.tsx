8df2fc2012bfbe5c2203f4abefd6b9a168aaa9fc
Fatemeh Khosravi <uni.khosravi@gmail.com>
2020-10-13 11:08:16 +0330

    feat: using slider color component

--- src/ColorToner.tsx
+++ src/ColorToner.tsx
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TextboxColor from "./components/TextboxColor";
 import SliderColor from "./components/SliderColor";
 import ListColor from "./components/ListColor";
-import { getColorToners } from "./utils";
+import { getColorToners, getSliceList } from "./utils";
 
 function ColorToner() {
   const [colorList, setColorList] = useState<string[]>([]);
+  const [selectedColorList, setSelectedColorList] = useState<string[]>([]);
 
   return (
     <div>
       <TextboxColor callback={(color) => setColorList(getColorToners(color))} />
-      <SliderColor />
+      <SliderColor
+        callback={(range) =>
+          setSelectedColorList(getSliceList(colorList, range))
+        }
+      />
       <ListColor />
     </div>
   );
