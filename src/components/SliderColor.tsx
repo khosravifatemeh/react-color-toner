9f05b0d9c3d2e4e516f1d495ea543afdc8dafae8
Fatemeh Khosravi <uni.khosravi@gmail.com>
2020-10-13 11:07:54 +0330

    feat: handle slider change

--- src/components/SliderColor.tsx
+++ src/components/SliderColor.tsx
@@ -1,5 +1,21 @@
-import React from "react";
-const SliderColor = () => {
-  return <div></div>;
+import React, { useState } from "react";
+type Props = {
+  callback: (range: number) => void;
+};
+const SliderColor: React.FC<Props> = ({ callback }) => {
+  const [value, setValue] = useState("");
+  function handleChange(e: any) {
+    setValue(e.target.value);
+    return callback(e.target.value);
+  }
+  return (
+    <input
+      type="range"
+      min="1"
+      max="40"
+      value={value}
+      onChange={(e) => handleChange(e)}
+    />
+  );
 };
 export default SliderColor;
