3686ab07767a6c9f254f2c726006f1da3c149069
Fatemeh Khosravi <uni.khosravi@gmail.com>
2020-10-14 08:48:46 +0330

    fix: using handle change method instead useeffect

--- src/ColorToner.tsx
+++ src/ColorToner.tsx
@@ -7,30 +7,13 @@ import "./ColorToner.scss";
 
 const ColorToner: React.FC = () => {
   const [colorList, setColorList] = useState<string[]>([]);
-  const [selectedColorList, setSelectedColorList] = useState<string[]>([]);
   const [range, setRange] = useState<number>(1);
-  const [validColor, setValidColor] = useState("");
-
-  useEffect(() => {
-    if (!colorList.length) return;
-    return setSelectedColorList(getSliceList(colorList, range));
-  }, [range]);
-
-  useEffect(() => {
-    if (!validColor) return;
-    return setColorList(getColorToners(validColor, "#000000", 40));
-  }, [validColor]);
-
-  useEffect(() => {
-    if (!validColor || !colorList.length) return;
-    return setSelectedColorList(getSliceList(colorList, range));
-  }, [validColor]);
 
   return (
     <div className="container">
-      <TextboxColor callback={(color) => setValidColor(color)} />
+      <TextboxColor callback={(color) => setColorList(getColorToners(color))} />
       <SliderColor value={range} callback={(range) => setRange(range)} />
-      <ListColor items={selectedColorList} />
+      <ListColor items={getSliceList(colorList, range)} />
     </div>
   );
 };
